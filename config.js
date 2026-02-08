4d913afa59d424caff6400fbf50c17e4ba183f3d
salhabassad-afk <salhabassad@gmail.com>
2026-02-08 15:07:02 +0400

    Update config.js
    3.0

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "are you in love with me and willing to sacrafise anything for our reltionship?",                                    // First interaction
+            text: "do you like me princess?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
